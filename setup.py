ad8e2d14362937da49660716a257bffbd82313bd
adithyaadii12 <kvrdadithya@gmail.com>
2024-09-21 01:04:22 +0530

    set up file

---
+++ setup.py
@@ -0,0 +1,29 @@
+from setuptools import find_packages, setup
+import setuptools
+
+with open("README.md", "r", encoding="utf-8") as f:
+    long_description = f.read()
+
+
+__version__ = "0.0.0"
+
+REPO_NAME = "backorder"
+AUTHOR_USER_NAME = "adithyaadii12"
+SRC_REPO = "backorder_project"
+AUTHOR_EMAIL = "kvrdadithya@gmail.com"
+
+
+setup(
+    name=SRC_REPO,
+    version=__version__,
+    author=AUTHOR_USER_NAME,
+    author_email=AUTHOR_EMAIL,
+    description="back order prediction",
+    long_description="long_description",
+    url=f"https://github.com/{AUTHOR_USER_NAME}/{REPO_NAME}",
+    project_urls={
+        "Bug Tracker": f"https://github.com/{AUTHOR_USER_NAME}/{REPO_NAME}/issues",
+    },
+    package_dir={"": "backorder_project"},
+    packages=setuptools.find_packages(where="backorder_project")
+)
