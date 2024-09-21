1f46d4127a1f7bf30d9754ae65e499d67692e4c0
adithyaadii12 <kvrdadithya@gmail.com>
2024-09-22 04:21:35 +0530

    modifying

--- setup.py
+++ setup.py
@@ -24,6 +24,6 @@ setup(
     project_urls={
         "Bug Tracker": f"https://github.com/{AUTHOR_USER_NAME}/{REPO_NAME}/issues",
     },
-    package_dir={"": "backorder_project"},
-    packages=setuptools.find_packages(where="backorder_project")
+    package_dir={"": "src"},
+    packages=setuptools.find_packages(where="src")
 )
